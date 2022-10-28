79cf5798581161c92732675bbc8747e73f350ba1
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-10-28 21:42:53 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,18 +5,30 @@ import Contacts from '../Contacts/Contacts';
 import Form from '../Form/Form';
 import Filter from '../Filter/Filter';
 
-
-class App extends Component {
-  state = {
-    contacts: [
+const initialContacts = [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    ]
+
+class App extends Component {
+  state = {
+    contacts: [],
     filter: ''
   };
 
+  componentDidMount() {
+    const data = localStorage.getItem('contacts') ? JSON.parse(localStorage.getItem('contacts')) : initialContacts;
+    this.setState({ contacts: data });
+  }
+
+  componentDidUpdate(prevProps, prevState) { 
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  
     onAddContact = (name, number) => {
     const newContact = this.state.contacts.find(el => el.name.toLowerCase() === name.toLowerCase())
     if (newContact) {
---
+++ src/components/Storage/Storage.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import Contacts from '../Contacts/Contacts';
+
+
+
+
+
+
+
+
+
+const saveContacts = (key, contacts) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export default {
+  save,
+  load,
+};
